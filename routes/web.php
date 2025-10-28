438d6dfc6bfbbecbaeb96d76ad63dc6c4481d1c1
ZikraZana <monozikk13@gmail.com>
2025-10-28 21:13:46 +0700

    fix web routes

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,23 @@
 <?php
 
+use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Route;
 
+//========================= AREA ALL =========================//
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/dashboard', function () {
-    return view('dashboard.index');
-})->middleware('auth')->name('dashboard');
 
-Route::get('/login', [App\Http\Controllers\LoginController::class, 'showLoginForm'])->name('login.form');
-Route::post('/login', [App\Http\Controllers\LoginController::class, 'login'])->name('login');
-Route::post('/logout', [App\Http\Controllers\LoginController::class, 'logout'])->name('logout');
+//========================= AREA GUEST =========================//
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [App\Http\Controllers\LoginController::class, 'showLoginForm'])->name('login.form');
+    Route::post('/login', [App\Http\Controllers\LoginController::class, 'login'])->name('login');
+});
+
+//========================= AREA Admins =========================//
+Route::middleware('auth')->group(function () {
+    Route::post('/logout', [App\Http\Controllers\LoginController::class, 'logout'])->name('logout');
+    Route::get('/dashboard', function () {
+        return view('dashboard.index');
+    })->name('dashboard');
+});
